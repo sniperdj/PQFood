dfa41cd136da0a367a5228449e7cb42b87d13213
Sniper <sniper_zheng@163.com>
2021-03-05 19:53:56 +0800

    commit

---
+++ pq-food/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <pqtabbar></pqtabbar>
+  </div>
+</template>
+
+<script>
+
+import pqtabbar from './components/base-ui/pqtabbar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    pqtabbar
+  }
+}
+</script>
+
+<style>
+/* @import "~@/style/normalize.css" */
+</style>
